3bb167197e39a4a36f0456dc453f5e214f8defa7
NIASKA <mkman751@yahoo.com>
2021-08-14 16:12:26 -0500

    initial push to heroku

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from './Home/Home'
 export {default as AboutUs} from './AboutUs/AboutUs'
 export {default as Location} from './Location/Location'
+export {default as Menu} from './Menu/Menu'
